6dc3dd6ffcb110cd5fb46b4c62f27137b7f20456
LeKeno <33626494+lekeno@users.noreply.github.com>
2021-12-22 17:28:35 +0900

    Derived presence stats

--- edrpresence.js
+++ edrpresence.js
@@ -9,21 +9,60 @@ module.exports = class EDRPresence {
         var currentMonth = new Date().getUTCMonth();
         var orderly = this._orderlyMonthNo();
         var ships = {};
-
+        this.days = {"total": 0, "max": 0, "counts":[0,0,0,0,0,0,0]};
+        this.hours = {"total": 0, "max": 0, "counts":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]};
+        this.months = {"total": 0, "max": 0, "counts": [], "byday":[], "byhour": []};
         orderly.forEach(m => {
+            var byday = [0,0,0,0,0,0,0];
+            var byhour = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
             if (!(m in presenceStats))  {
+                this.months["counts"].push(0);
+                this.months["byday"].push(byday);
+                this.months["byhour"].push(byhour);
                 return;
             }
 
             var wayTooOld = parseInt(presenceStats[m]["year"]) < currentYear-1;
             var tooOld = (parseInt(presenceStats[m]["year"]) == currentYear-1) && m <= currentMonth;
             if (wayTooOld || tooOld) {
+                this.months["counts"].push(0);
+                this.months["byday"].push(byday);
+                this.months["byhour"].push(byhour);
                 return;
             }
 
             ships = this._sumObjectsByKey(ships, presenceStats[m]["ships"]);
+            delete ships["Unknown"];
+            for (var i = 0; i<12; i++) {
+                if (presenceStats[m] == undefined || presenceStats[m]["days"] == undefined || presenceStats[m]["days"][i] == undefined) continue;
+                var count = presenceStats[m]["days"][i] || presenceStats[m]["days"][i.toString()];
+                if (count == undefined) { count = 0;}
+                this.days["counts"][i] += count;
+                this.days["total"] += count;
+                this.days["max"] = Math.max(this.days["counts"][i], this.days["max"]);
+                byday[i] += count;
+            }
+            this.months["byday"].push(byday);
+            
+            for (var i = 0; i<24; i++) {
+                if (presenceStats[m] == undefined || presenceStats[m]["times"] == undefined || presenceStats[m]["times"][i] == undefined) continue;
+                var count = presenceStats[m]["times"][i] || presenceStats[m]["times"][i.toString()];
+                if (count == undefined) { count = 0;}
+                this.hours["counts"][i] += count;
+                this.hours["total"] += count;
+                this.hours["max"] = Math.max(this.hours["counts"][i], this.hours["max"]);
+                byhour[i] += count
+            }
+            this.months["byhour"].push(byhour);
+            
+            var count = presenceStats[m]["total"];
+            if (count == undefined) {
+                count= 0;
+            }
+            this.months["counts"].push(count);
+            this.months["total"] += count;
+            this.months["max"] = Math.max(count, this.months["max"]);
         });
-
         this.totalShips = Object.keys(ships).reduce((sum,key)=>sum+(ships[key]||0),0);
         this.topShips =  Object.keys(ships).map(function(key) {
             return [key, ships[key]];
@@ -36,8 +75,8 @@ module.exports = class EDRPresence {
         
     }
 
-    visualization() {
-        return sparkline.shipsgraph(this.topShips, this.totalShips);
+    async visualization() {
+        return [sparkline.shipsgraph(this.topShips, this.totalShips), await sparkline.presencegraph(this.months, this.days, this.hours)]
     }
 
     _orderlyMonthNo() {
--- sparkline.js
+++ sparkline.js
@@ -1,5 +1,5 @@
 'use strict';
-const { createCanvas } = require('canvas');
+const { createCanvas, loadImage } = require('canvas');
 const utils = require('./utils');
 
 var self = module.exports = {
@@ -29,7 +29,7 @@ var self = module.exports = {
         const startMonth = (month+1+12)%12;
         const startYear = (startMonth > month) ? year-1 : year; 
 
-        ctx.fillStyle = 'rgb(247, 247, 247)';
+        ctx.fillStyle = 'rgb(255, 255, 255)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.font = '9px sans-serif';
         ctx.textAlign = 'end';
@@ -58,11 +58,10 @@ var self = module.exports = {
         for (i = 0; i < total; i = i + 1) {
             if (clean[i] > 0) {
                 var dx = 104, dy = 16;
-                h = clean[i]/ystep;
+                h = Math.max(clean[i]/ystep, 1);
                 y = height - h;
                 ctx.beginPath();
                 ctx.rect(x+dx, y+dy, w, h);
-                console.log(`v:${clean[i]}, x:${x+dx}, y:${y+dy}, w: ${w}, h: ${h}`);
                 ctx.fillStyle = 'rgb(52, 158, 129)';
                 ctx.fill();
                 ctx.closePath();
@@ -70,11 +69,10 @@ var self = module.exports = {
 
             if (wanted[i] > 0) {
                 var dx = 104, dy = 37;
-                h = wanted[i]/ystep;
+                h = Math.max(wanted[i]/ystep, 1);
                 y = 0;
                 ctx.beginPath();
                 ctx.rect(x+dx, y+dy, w, h);
-                console.log(`v:${wanted[i]}, x:${x+dx}, y:${y+dy}, w: ${w}, h: ${h}`);
                 ctx.fillStyle = 'rgb(31,38,42)';
                 ctx.fill();
                 ctx.closePath();
@@ -82,11 +80,10 @@ var self = module.exports = {
 
             if (bounties[i] > 0) {
                 var dx = 104, dy = 58;
-                h = bounties[i]/ystepb;
+                h = Math.max(bounties[i]/ystepb, 1);
                 y = height - h;
                 ctx.beginPath();
                 ctx.rect(x+dx, y+dy, w, h);
-                console.log(`v:${wanted[i]}, x:${x+dx}, y:${y+dy}, w: ${w}, h: ${h}`);
                 ctx.fillStyle = self._wantedcolor(bounties[i]);
                 ctx.fill();
                 ctx.closePath();
@@ -112,7 +109,7 @@ var self = module.exports = {
             h = 12,
             i;
 
-        ctx.fillStyle = 'rgb(247, 247, 247)';
+        ctx.fillStyle = 'rgb(255, 255, 255)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.font = 'bold 9px sans-serif';
         ctx.textAlign = 'end';
@@ -131,7 +128,6 @@ var self = module.exports = {
                 w = ships[i][1]/totalShips * width;
                 ctx.beginPath();
                 ctx.rect(x+dx, y-ystep/2.0-h/2.0+dy, w, h);
-                console.log(`l:${ships[i][0]}, v:${ships[i][1]}, x:${x+dx}, y:${y-h}, w: ${w}, h: ${h}`);
                 ctx.fillStyle = 'rgb(10, 139, 214)';
                 ctx.fill();
                 ctx.closePath();
@@ -171,7 +167,6 @@ var self = module.exports = {
                 w = rolesStats[role]/totalShips * width;
                 ctx.beginPath();
                 ctx.rect(x+dx, y-ystep/2.0-h/2.0+dy, w, h);
-                console.log(`l:${role}, v:${rolesStats[role]}, x:${x+dx}, y:${y-h}, w: ${w}, h: ${h}`);
                 ctx.fill();
                 ctx.closePath();
             }
@@ -212,6 +207,164 @@ var self = module.exports = {
         return canvas;
     },
 
+    drawImage: async function(path) {
+        const localImage = await loadImage(path);
+        return localImage;
+    },
+
+    presencegraph: async function (months, days, hours) {
+        const localImage = await loadImage("./img/presence-template.png");
+        
+        var canvas = createCanvas(390, 550);
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(localImage, 0, 0);
+        
+        var height = 9,
+            width = 21,
+            total = 12,
+            startx = 110,
+            starty = 53,
+            xstep = width+1,
+            ystep = 0,
+            x = startx,
+            y = starty,
+            w = width,
+            h = height,
+            i;
+
+        for (i = 0; i < total; i = i + 1) {
+            ctx.beginPath();
+            ctx.rect(x, y, w, h);
+            ctx.fillStyle = this._presencecolor(months["counts"][i], months["max"]);
+            ctx.fill();
+            ctx.closePath();
+            x = x + xstep;
+            y = y + ystep;
+        }
+
+        height = 20;
+        width = 10;
+        total = 7;
+        startx = 99;
+        starty = 63;
+        xstep = 0;
+        ystep = height+1;
+        x = startx;
+        y = starty;
+        w = width;
+        h = height;
+
+        for (i = 0; i < total; i = i + 1) {
+            ctx.beginPath();
+            ctx.rect(x, y, w, h);
+            ctx.fillStyle = this._presencecolor(days["counts"][i], days["max"]);
+            ctx.fill();
+            ctx.closePath();
+            x = x + xstep;
+            y = y + ystep;
+        }
+
+        height = 20;
+        width = 10;
+        total = 12;
+        startx = 99;
+        starty = 283;
+        xstep = 0;
+        ystep = height+1;
+        x = startx;
+        y = starty;
+        w = width;
+        h = height;
+
+        for (i = 0; i < total; i = i + 1) {
+            ctx.beginPath();
+            ctx.rect(x, y, w, h);
+            ctx.fillStyle = this._presencecolor(months["counts"][i], months["max"]);
+            ctx.fill();
+            ctx.closePath();
+            x = x + xstep;
+            y = y + ystep;
+        }
+
+        height = 9;
+        width = 10;
+        total = 24;
+        startx = 110;
+        starty = 273;
+        xstep = width+1;
+        ystep = 0;
+        x = startx;
+        y = starty;
+        w = width;
+        h = height;
+
+        for (i = 0; i < total; i = i + 1) {
+            ctx.beginPath();
+            ctx.rect(x, y, w, h);
+            ctx.fillStyle = this._presencecolor(hours["counts"][i], hours["max"]);
+            ctx.fill();
+            ctx.closePath();
+            x = x + xstep;
+            y = y + ystep;
+        }
+
+        height = 20;
+        width = 21;
+        total = 12;
+        startx = 110;
+        starty = 63;
+        xstep = width+1;
+        ystep = height+1;
+        x = startx;
+        y = starty;
+        w = width;
+        h = height;
+
+        for (i=0; i< total; i = i + 1) {
+            var byday = months["byday"][i];
+            var max = months["max"];
+            for (var j=0; j<byday.length; j = j +1) {
+                ctx.beginPath();
+                ctx.rect(x, y, w, h);
+                ctx.fillStyle = this._presencecolor(byday[j], max);
+                ctx.fill();
+                ctx.closePath();
+                y = y + ystep;
+            }
+            x = x + xstep;
+            y = starty;
+        }
+
+        height = 20;
+        width = 10;
+        total = 12;
+        startx = 110;
+        starty = 283;
+        xstep = width+1;
+        ystep = height+1;
+        x = startx;
+        y = starty;
+        w = width;
+        h = height;
+
+        for (i=0; i< total; i = i + 1) {
+            var byhour = months["byhour"][i];
+            var max = months["max"];
+            for (var j=0; j<byhour.length; j = j +1) {
+                ctx.beginPath();
+                ctx.rect(x, y, w, h);
+                ctx.fillStyle = this._presencecolor(byhour[j], max);
+                ctx.fill();
+                ctx.closePath();
+                x = x + xstep;
+            }
+            y = y + ystep;
+            x = startx;
+        }
+
+        return canvas;
+    },
+
     _wantedcolor: function (bounty) {        
         if (bounty <= 1000) {
             return 'rgb(141, 130, 115)';
@@ -231,5 +384,11 @@ var self = module.exports = {
             return 'rgb(255,0,0)';
         }
         return 'rgb(155, 0, 250)';
+    },
+
+    _presencecolor: function (count, maxcount) {
+        if (maxcount <= 0 || count <= 0) return 'rgb(229, 229, 229)';
+        var percent = count / maxcount;
+        return `hsl(202, 91%, ${50+50*(1.0-percent)}%)`;
     }
 };
